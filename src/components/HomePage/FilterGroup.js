84dfff41d289dff1e9fa5ff1d8bbe1150f200727
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-05-13 23:40:11 +0700

    update filter group ui

--- src/components/HomePage/FilterGroup.js
+++ src/components/HomePage/FilterGroup.js
@@ -1,69 +1,79 @@
-import { Box, makeStyles, Typography } from "@material-ui/core";
-import { ToggleButton, ToggleButtonGroup } from "@mui/material";
-import { green } from "@material-ui/core/colors";
+import {Box, makeStyles, Typography, withStyles} from "@material-ui/core";
+import {green} from "@material-ui/core/colors";
+import {ToggleButton, ToggleButtonGroup} from "@material-ui/lab";
 
-const useStyles = makeStyles((theme) => ({
-  buttonGroup: {
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-    fontWeight: "bold",
-    fontSize: "1rem",
-    lineHeight: 1.75,
-    letterSpacing: "0.00938em",
-    color: theme.palette.primary.main,
-    textTransform: "none",
-    borderRadius: 15,
-    padding: theme.spacing(0, 2),
-    marginLeft: theme.spacing(2),
-    "&. selected": {
-      backgroundColor: theme.palette.primary.main,
-      "&. label": {
-        color: "#fff",
-        // text gradient
-        backgroundImage: `linear-gradient(to right, ${green[100]}, ${green["A400"]})`,
-        backgroundClip: "text",
-        WebkitBackgroundClip: "text",
-        WebkitTextFillColor: "transparent",
+const StyledToggleButton = withStyles((theme) => (
+	{
+		root: {
+			// Typography subtitle1
+			fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+			fontWeight: "bold",
+			fontSize: "1rem",
+			lineHeight: 1.75,
+			letterSpacing: "0.00938em",
 
-        // cho safari khong nhan WebkitBackgroundClip
-        display: "inline",
-      },
-      "&:hover": {
-        backgroundColor: theme.palette.primary.main,
-      },
-    },
-  },
-}));
+			color: theme.palette.primary.main,
+			textTransform: "none",
+			borderRadius: 15,
+			padding: "0px 16px",
+			marginLeft: 16,
+			"&$selected": {
+				backgroundColor: theme.palette.primary.main,
+				"& $label": {
+					color: "#fff",
+					// text gradient
+					backgroundImage: `linear-gradient(to right, ${green[100]}, ${green["A400"]})`,
+					backgroundClip: "text",
+					WebkitBackgroundClip: "text",
+					WebkitTextFillColor: "transparent",
+
+					// cho safari khong nhan WebkitBackgroundClip
+					display: "inline",
+				},
+				"&:hover": {
+					backgroundColor: theme.palette.primary.main,
+				},
+			},
+		},
+		selected: {},
+		label: {},
+	}
+))(ToggleButton);
+
+
+const useStyles = makeStyles((theme) => (
+	{}
+));
 
 const FilterGroup = ({
-  title,
-  value,
-  handleChange,
-  value1,
-  value2,
-  label1,
-  label2,
-}) => {
-  const classes = useStyles();
-  return (
-    <Box display="flex" alignItems="center" paddingLeft="40px" width="100%">
-      <Typography variant="h6">{title}</Typography>
-      <ToggleButtonGroup
-        value={value}
-        exclusive
-        onChange={handleChange}
-        className={classes.buttonGroup}
-        size="small"
+	                     title,
+	                     value,
+	                     handleChange,
+	                     value1,
+	                     value2,
+	                     label1,
+	                     label2,
+                     }) => {
+	const classes = useStyles();
+	return (
+		<Box display="flex" alignItems="center" paddingLeft="40px" width="100%">
+			<Typography variant="h6">{title}</Typography>
+			<ToggleButtonGroup
+				value={value}
+				exclusive
+				onChange={handleChange}
+				size="small"
 
-      >
-        <ToggleButton value={value1} sx={{ fontWeight: "600" }}>
-          {label1}
-        </ToggleButton>
-        <ToggleButton value={value2} sx={{ fontWeight: "600" }}>
-          {label2}
-        </ToggleButton>
-      </ToggleButtonGroup>
-    </Box>
-  );
+			>
+				<StyledToggleButton value={value1}>
+					{label1}
+				</StyledToggleButton>
+				<StyledToggleButton value={value2}>
+					{label2}
+				</StyledToggleButton>
+			</ToggleButtonGroup>
+		</Box>
+	);
 };
 
 export default FilterGroup;
--- src/components/HomePage/JoinToday.js
+++ src/components/HomePage/JoinToday.js
@@ -1,99 +1,96 @@
-import {
-  Box,
-  Button,
-  Grid,
-  Typography,
-  alpha,
-  makeStyles,
-} from "@material-ui/core";
-import { purple } from "@material-ui/core/colors";
+import {alpha, Box, Button, Grid, makeStyles, Typography,} from "@material-ui/core";
+import {purple} from "@material-ui/core/colors";
 import join_today from "../../assets/join_today.jpeg";
 
-const useStyles = makeStyles((theme) => ({
-  joinToday: {
-    marginTop: theme.spacing(3),
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "space-around",
-    flexDirection: "column",
-    flexWrap: "wrap",
-    maxWidth: 1300,
-    // Something wrong here
-    backgroundImage: `url(${join_today})`,
-    color: theme.palette.getContrastText(purple[800]),
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    minHeight: 300,
-  },
-  backdrop: {
-    backgroundImage: `linear-gradient(
+const useStyles = makeStyles((theme) => (
+	{
+		joinToday: {
+			marginTop: theme.spacing(3),
+			padding: theme.spacing(0, 6),
+
+			width: "100%",
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "space-around",
+			flexDirection: "column",
+			flexWrap: "wrap",
+			maxWidth: 1300,
+			// Something wrong here
+			backgroundImage: `url(${join_today})`,
+			color: theme.palette.getContrastText(purple[800]),
+			backgroundPosition: "center",
+			backgroundSize: "cover",
+			minHeight: 300,
+		},
+		backdrop: {
+			backgroundImage: `linear-gradient(
       ${alpha(purple[800], 0.5)}, 
       ${alpha(purple[800], 0.5)}),
       url(${join_today})`,
-    color: theme.palette.getContrastText(purple[800]),
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    minHeight: 300,
-  },
-  button: {
-    marginTop: theme.spacing(3),
-    backgroundColor: "rgba(128,91,231, 1)",
-    color: theme.palette.getContrastText(purple[800]),
-    "&:hover": {
-      backgroundColor: "transparent",
-    },
-  },
-  ul: {
-    marginLeft: 20,
-    padding: "0 20px",
-    listStyleType: "circle",
-    opacity: 0.8,
-  },
-}));
+			color: theme.palette.getContrastText(purple[800]),
+			backgroundPosition: "center",
+			backgroundSize: "cover",
+			minHeight: 300,
+		},
+		button: {
+			marginTop: theme.spacing(3),
+			backgroundColor: "rgba(128,91,231, 1)",
+			color: theme.palette.getContrastText(purple[800]),
+			"&:hover": {
+				backgroundColor: "transparent",
+			},
+		},
+		ul: {
+			marginLeft: 20,
+			padding: "0 20px",
+			listStyleType: "circle",
+			opacity: 0.8,
+		},
+	}
+));
 
 const JoinToday = () => {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <div className={classes.joinToday}>
-      <Box style={{ paddingLeft: "38px" }}>
-        <Typography variant="h4">Join Today</Typography>
-      </Box>
+	return (
+		<div className={classes.joinToday}>
+			<Box style={{paddingLeft: "38px"}}>
+				<Typography variant="h4">Join Today</Typography>
+			</Box>
 
-      <Grid container spacing={3}>
-        <Grid
-          item
-          md={7}
-          xs={12}
-          style={{ paddingLeft: "48px", paddingTop: "20px" }}
-        >
-          <Typography>
-            Get access to maintain your own custom personal lists, track what
-            you've seen and search and filter for what to watch next—regardless
-            if it's in theatres, on TV or available on popular streaming
-            services like .
-          </Typography>
-          <Button variant="contained" className={classes.button}>
-            Sign up
-          </Button>
-        </Grid>
+			<Grid container spacing={3}>
+				<Grid
+					item
+					md={7}
+					xs={12}
+					style={{paddingLeft: "48px", paddingTop: "20px"}}
+				>
+					<Typography>
+						Get access to maintain your own custom personal lists, track what
+						you've seen and search and filter for what to watch next—regardless
+						if it's in theatres, on TV or available on popular streaming
+						services like .
+					</Typography>
+					<Button variant="contained" className={classes.button}>
+						Sign up
+					</Button>
+				</Grid>
 
-        <Grid item md={5} style={{ paddingTop: "10px" }}>
-          <ul className={classes.ul}>
-            <Typography>Enjoy TMDB ad free</Typography>
-            <Typography>Maintain a personal watchlist</Typography>
-            <Typography>
-              Filter by your subscribed streaming services and find something to
-              watch
-            </Typography>
-            <Typography>Log the movies and TV shows you've seen</Typography>
-            <Typography>Build custom lists</Typography>
-            <Typography>Contribute to and improve our database</Typography>
-          </ul>
-        </Grid>
-      </Grid>
-    </div>
-  );
+				<Grid item md={5} style={{paddingTop: "10px"}}>
+					<ul className={classes.ul}>
+						<Typography>Enjoy TMDB ad free</Typography>
+						<Typography>Maintain a personal watchlist</Typography>
+						<Typography>
+							Filter by your subscribed streaming services and find something to
+							watch
+						</Typography>
+						<Typography>Log the movies and TV shows you've seen</Typography>
+						<Typography>Build custom lists</Typography>
+						<Typography>Contribute to and improve our database</Typography>
+					</ul>
+				</Grid>
+			</Grid>
+		</div>
+	);
 };
 export default JoinToday;
