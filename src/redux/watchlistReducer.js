3642561f09487c0e347308533483dda6f26f01c4
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-05-17 17:29:56 +0700

    watchlist reducers

---
+++ src/redux/watchlistReducer.js
@@ -0,0 +1,80 @@
+import {
+    ADD_TO_WATCHLIST_FAILURE,
+    ADD_TO_WATCHLIST_REQUEST,
+    ADD_TO_WATCHLIST_SUCCESS,
+    GET_WATCHLIST_FAILURE,
+    GET_WATCHLIST_REQUEST,
+    GET_WATCHLIST_SUCCESS,
+    REMOVE_FROM_WATCHLIST_FAILURE,
+    REMOVE_FROM_WATCHLIST_REQUEST,
+    REMOVE_FROM_WATCHLIST_SUCCESS
+} from "./types";
+
+
+const initialState = {
+    watchlist: [],
+    isAdding: false,
+    isLoading: false,
+    isRemoving: null,
+}
+
+const watchlistReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_WATCHLIST_REQUEST:
+            return {
+                ...state,
+                isLoading: true,
+            };
+        case GET_WATCHLIST_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                watchlist: action.watchlist,
+            };
+
+        case GET_WATCHLIST_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+            };
+
+        case ADD_TO_WATCHLIST_REQUEST :
+            return {
+                ...state,
+                isAdding: true,
+
+            };
+        case ADD_TO_WATCHLIST_SUCCESS :
+            return {
+                ...state,
+                isAdding: false,
+                watchlist: [action.movie, ...state.watchlist],
+            };
+        case ADD_TO_WATCHLIST_FAILURE:
+            return {
+                ...state,
+                isAdding: false,
+
+            };
+
+        case REMOVE_FROM_WATCHLIST_REQUEST:
+            return {
+                ...state,
+                isRemoving: action.id
+            };
+        case REMOVE_FROM_WATCHLIST_SUCCESS:
+            return {
+                ...state,
+                isRemoving: null,
+                watchlist: state.watchlist.filter((movie) => movie.id == action.id)
+            };
+        case REMOVE_FROM_WATCHLIST_FAILURE :
+            return {
+                ...state,
+                isRemoving: null,
+            };
+        default:
+            return state;
+    }
+}
+export default watchlistReducer;
