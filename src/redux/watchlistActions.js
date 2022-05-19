e5155b63ade5635787b889f476ac59f16cc67f4e
Nguyen Minh Khang <38356205+khangnguyen9955@users.noreply.github.com>
2022-05-19 17:32:54 +0700

    watch list actions

---
+++ src/redux/watchlistActions.js
@@ -0,0 +1,21 @@
+export const auth = () => {
+    {
+
+
+    }
+
+
+}
+
+
+export const addWatchlist = () => async (dispatch) => {
+    dispatch();
+    try {
+
+
+    } catch (err) {
+        dispatch();
+    }
+
+
+}
